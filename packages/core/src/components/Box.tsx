c5c0717413c4cdf7ce36f6e5f0e09b1806aa8140
ebadgio <eb27@rice.edu>
2019-11-17 14:49:18 -0800

    intial monorepo and core package setup

---
+++ packages/core/src/components/Box.tsx
@@ -0,0 +1,10 @@
+import { space, layout, color } from 'styled-system';
+import styled from '@emotion/styled';
+
+const Box = styled('div')`
+  ${space}
+  ${layout}
+  ${color}
+`;
+
+export default Box;
