97aa388c12486637de52dadb433dbbcd165305c8
ebadgio <eb27@rice.edu>
2020-01-04 18:02:12 -0800

    add tests for Button

---
+++ packages/core/tests/button.test.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import { mount } from 'enzyme';
+import { Button } from '../src/components/Button';
+import { ThemeProvider } from '../src/components/ThemeProvider';
+import { BaseTheme } from '../src/config/theme';
+
+describe('Button', () => {
+  it('should render properly with correct props and children', () => {
+    const wrapper = mount(
+      <ThemeProvider theme={BaseTheme}>
+        <Button>Hello</Button>
+      </ThemeProvider>
+    );
+    expect(
+      wrapper
+        .find(Button)
+        .first()
+        .prop('variant')
+    ).toEqual('primary');
+    expect(
+      wrapper
+        .find('button')
+        .first()
+        .hasClass(/(css-).+/)
+    ).toEqual(true);
+    expect(
+      wrapper
+        .find('button')
+        .first()
+        .text()
+    ).toEqual('Hello');
+  });
+
+  it('should execute onClick function when click event is triggered', () => {
+    const clickFunc = jest.fn(() => {});
+    const wrapper = mount(
+      <ThemeProvider theme={BaseTheme}>
+        <Button onClick={clickFunc}>Hello</Button>
+      </ThemeProvider>
+    );
+    wrapper
+      .find(Button)
+      .first()
+      .simulate('click');
+    expect(clickFunc).toHaveBeenCalled();
+  });
+});
