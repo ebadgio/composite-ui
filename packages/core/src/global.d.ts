f962727363365051a9a72dcf2c492ba5151845c9
ebadgio <eb27@rice.edu>
2020-02-17 22:13:59 -0800

    fix global and variant typings

--- packages/core/src/global.d.ts
+++ packages/core/src/global.d.ts
@@ -1,8 +1,1 @@
-type Merge<L, R> = R & Pick<L, Exclude<keyof L, keyof R>>;
-declare module 'enzyme-adapter-react-16';
-
-/* Styled system types */
-type ResponsiveValue<T> = T | Array<T | null> | { [key: string]: T };
-interface VariantStyleProps {
-  variant?: ResponsiveValue<string>;
-}
+declare type Merge<L, R> = R & Pick<L, Exclude<keyof L, keyof R>>;
